f8e530bcbeaefaf8327fd0dd9cbdc4a72791150e
ElijahHarris <47341441+ElijahHarris@users.noreply.github.com>
2020-07-17 09:44:21 -0600

    Add food controller and API connection method

---
+++ LunchCalculator/Controllers/FoodController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LunchCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace LunchCalculator.Controllers
+{
+    public class FoodController : Controller
+    {  
+        public IActionResult SearchFood()
+        {
+            RestClient client = new RestClient("https://api.yelp.com/v3businesses/search?term=delis");
+            RestRequest request = new RestRequest(Method.POST); ;
+            request.AddHeader("Authorization", "Bearer v1AI_S2LzYgDRy37YwJL1idcwfOCHr9DDGv58kvOUUEIxdJDU-QInbxpBzmXtYiihq5ZZa3tlWhOQzMHRjrRd-pZzw32ChpU44ZoEEdyP_ty6PYlJOQsUZ2MjWYQX3Yx");
+            request.AddParameter("location", "SaltLakeCity");
+            IRestResponse response = client.Execute(request);
+            if (response.IsSuccessful)
+            {
+                SearchResult json = Newtonsoft.Json.JsonConvert.DeserializeObject<SearchResult>(response.Content);
+                return View(json);
+            }
+            else
+            {
+                Console.WriteLine("ERROR", (int)response.StatusCode, response.ErrorMessage);
+            }
+            return View();
+        }
+    }
+}
---
+++ LunchCalculator/Models/Resturant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LunchCalculator.Models
+{
+    public class Resturant
+    {
+        public string id { get; set; }
+        public string alias { get; set; }
+        public string name { get; set; }
+        public string image_url { get; set; }
+        public bool is_closed { get; set; }
+        public string url { get; set; }
+        public int review_count { get; set; }
+        public List<Category> categories { get; set; }
+        public double rating { get; set; }
+        public Coordinates coordinates { get; set; }
+        public List<string> transactions { get; set; }
+        public string price { get; set; }
+        public Location location { get; set; }
+        public string phone { get; set; }
+        public string display_phone { get; set; }
+        public double distance { get; set; }
+    }
+
+    public class Category
+    {
+        public string alias { get; set; }
+        public string title { get; set; }
+
+    }
+}
