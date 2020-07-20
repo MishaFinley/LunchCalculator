62d7fd6e600b77e6f73d901e417061e0a810db9b
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2020-07-20 09:22:37 -0600

    parsed

--- LunchCalculator/Controllers/FoodController.cs
+++ LunchCalculator/Controllers/FoodController.cs
@@ -16,8 +16,10 @@ namespace LunchCalculator.Controllers
             IRestResponse response = client.Execute(request);
             if (response.IsSuccessful)
             {
-                SearchResult json = SearchResult.FromJSON(response.Content);
-                return View(json);
+                string json = response.Content;
+                SearchResult result = SearchResult.FromJSON(response.Content);
+                ViewBag.result = result;
+                return View();
             }
             else
             {
